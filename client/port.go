30efbc0ad34504e5f86b6465df72596446106154
ISHIDA Wataru <ishida.wataru@lab.ntt.co.jp>
2016-06-24 08:34:17 +0000

    cli port

--- client/port.go
+++ client/port.go
@@ -17,6 +17,7 @@ package main
 
 import (
 	"strconv"
+	"strings"
 
 	"bytes"
 	log "github.com/Sirupsen/logrus"
@@ -27,6 +28,119 @@ import (
 )
 
 var portClient portservice.PortClient
+
+type Ability uint64
+
+const (
+	ABILITY_10MB_HD     Ability = 1 << 0
+	ABILITY_10MB_FD     Ability = 1 << 1
+	ABILITY_100MB_HD    Ability = 1 << 2
+	ABILITY_100MB_FD    Ability = 1 << 3
+	ABILITY_1000MB_HD   Ability = 1 << 4
+	ABILITY_1000MB_FD   Ability = 1 << 5
+	ABILITY_2500MB_HD   Ability = 1 << 6
+	ABILITY_2500MB_FD   Ability = 1 << 7
+	ABILITY_10GB_HD     Ability = 1 << 8
+	ABILITY_10GB_FD     Ability = 1 << 9
+	ABILITY_PAUSE_TX    Ability = 1 << 10
+	ABILITY_PAUSE_RX    Ability = 1 << 11
+	ABILITY_PAUSE_ASYMM Ability = 1 << 12
+	ABILITY_TBI         Ability = 1 << 13
+	ABILITY_MII         Ability = 1 << 14
+	ABILITY_GMII        Ability = 1 << 15
+	ABILITY_SGMII       Ability = 1 << 16
+	ABILITY_XGMII       Ability = 1 << 17
+	ABILITY_LB_MAC      Ability = 1 << 18
+	ABILITY_LB_NONE     Ability = 1 << 19
+	ABILITY_LB_PHY      Ability = 1 << 20
+	ABILITY_AN          Ability = 1 << 21
+	ABILITY_3000MB_HD   Ability = 1 << 29
+	ABILITY_3000MB_FD   Ability = 1 << 30
+	ABILITY_12GB_HD     Ability = 1 << 22
+	ABILITY_12GB_FD     Ability = 1 << 23
+	ABILITY_13GB_HD     Ability = 1 << 24
+	ABILITY_13GB_FD     Ability = 1 << 25
+	ABILITY_16GB_HD     Ability = 1 << 26
+	ABILITY_16GB_FD     Ability = 1 << 27
+	ABILITY_COMBO       Ability = 1 << 31
+)
+
+func (a Ability) String() string {
+	ss := make([]string, 0, 8)
+	if ABILITY_10MB_HD&a > 0 {
+		ss = append(ss, "10MB_HD")
+	}
+	if ABILITY_10MB_FD&a > 0 {
+		ss = append(ss, "10MB_FD")
+	}
+	if ABILITY_100MB_HD&a > 0 {
+		ss = append(ss, "100MB_HD")
+	}
+	if ABILITY_100MB_FD&a > 0 {
+		ss = append(ss, "100MB_FD")
+	}
+	if ABILITY_1000MB_HD&a > 0 {
+		ss = append(ss, "1000MB_HD")
+	}
+	if ABILITY_1000MB_FD&a > 0 {
+		ss = append(ss, "1000MB_FD")
+	}
+	if ABILITY_2500MB_HD&a > 0 {
+		ss = append(ss, "2500MB_HD")
+	}
+	if ABILITY_2500MB_FD&a > 0 {
+		ss = append(ss, "2500MB_FD")
+	}
+	if ABILITY_10GB_HD&a > 0 {
+		ss = append(ss, "10GB_HD")
+	}
+	if ABILITY_10GB_FD&a > 0 {
+		ss = append(ss, "10GB_FD")
+	}
+	if ABILITY_PAUSE_TX&a > 0 {
+		ss = append(ss, "PAUSE_TX")
+	}
+	if ABILITY_PAUSE_RX&a > 0 {
+		ss = append(ss, "PAUSE_RX")
+	}
+	if ABILITY_PAUSE_ASYMM&a > 0 {
+		ss = append(ss, "PAUSE_ASYMM")
+	}
+	if ABILITY_TBI&a > 0 {
+		ss = append(ss, "TBI")
+	}
+	if ABILITY_MII&a > 0 {
+		ss = append(ss, "MII")
+	}
+	if ABILITY_GMII&a > 0 {
+		ss = append(ss, "GMII")
+	}
+	if ABILITY_SGMII&a > 0 {
+		ss = append(ss, "SGMII")
+	}
+	if ABILITY_XGMII&a > 0 {
+		ss = append(ss, "XGMII")
+	}
+	if ABILITY_LB_MAC&a > 0 {
+		ss = append(ss, "LB_MAC")
+	}
+	if ABILITY_LB_NONE&a > 0 {
+		ss = append(ss, "LB_NONE")
+	}
+	if ABILITY_LB_PHY&a > 0 {
+		ss = append(ss, "LB_PHY")
+	}
+	if ABILITY_AN&a > 0 {
+		ss = append(ss, "AN")
+	}
+	if ABILITY_3000MB_HD&a > 0 {
+		ss = append(ss, "3000MB_HD")
+	}
+	if ABILITY_3000MB_FD&a > 0 {
+		ss = append(ss, "3000MB_FD")
+	}
+	return strings.Join(ss, "|")
+}
 
 func FormatPortConfig(c *port.PortConfig) string {
 	s := bytes.NewBuffer(make([]byte, 0, 64))
@@ -197,7 +311,7 @@ func NewPortCmd() *cobra.Command {
 			if err != nil {
 				log.Fatal(err)
 			}
-			log.Info(res)
+			log.Info("ability: ", Ability(res.Ability).String())
 		},
 	}
 
